73ee1bf11e50dbb8d6eb6616d0783abb9247235d
Fekete <brunohffekete@gmail.com>
2022-12-07 22:04:48 -0300

    feat: added repository functions

--- src/repositories/todo.repository.ts
+++ src/repositories/todo.repository.ts
@@ -1,15 +1,38 @@
 import database from "../database/mock-database.js";
+import { To_do } from "../protocols.js";
 
 function findMany(){
     return database;
+}
+
+function findOne(id: number){
+    return database.find((e)=>e.id===id)
+}
+
+function insertOne(todo: To_do_Creator){
+    database.push({
+        id: database[database.length-1].id + 1,
+        text: todo.text,
+        is_done: todo.is_done
+    })
+}
+
+function updateOne(todo: To_do,newTodo: To_do_Creator){
+    todo.text = newTodo.text;
+    todo.is_done = newTodo.is_done;
 }
 
 function cleanDb(){
     database.length = 0;
 }
 
+export type To_do_Creator = Omit <To_do,"id">
+
 const repository = {
     findMany,
+    findOne,
+    insertOne,
+    updateOne,
     cleanDb
 }
 
